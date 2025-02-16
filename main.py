27f465bdfb4e70dccd9d3029966e6c932207fc0c
abhinav23122021234 <abhi.sharma19jan@gmail.com>
2025-02-16 23:35:28 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+import turtle
+import pandas as pd
+from turtle import Turtle,Screen
+screen=turtle.Screen()
+screen.title("U.S> States Game")
+image="blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
 
+df=pd.read_csv("50_states.csv")
+states=df["state"].to_list()
+guessed_state=[]
+while len(guessed_state)<50:
+    answer_state=screen.textinput(title=f"{len(guessed_state)}/50 states correct",prompt="what's another states name").title()
+
+    if answer_state in states:
+        t=turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data=df["answer_state"]
+        t.goto(state_data.x.item() , state_data.y.item())
+        t.write(answer_state)
+        guessed_state.append(answer_state)
+
+
+
+screen.exitonclick()
